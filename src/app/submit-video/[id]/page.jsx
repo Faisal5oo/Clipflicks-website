62a0d1a209165395751e76c11ff52ead2a727cc5
Faisal5oo <faisalharoon500@gmail.com>
2025-04-18 15:43:29 +0500

    fix

--- src/app/submit-video/[id]/page.jsx
+++ src/app/submit-video/[id]/page.jsx
@@ -36,200 +36,12 @@ export default function VideoSubmissionForm() {
   const signRef = useRef(null);
   console.log("Sign ref:", signRef);
 
-  const countries = [
-    { name: "Afghanistan" },
-    { name: "Albania" },
-    { name: "Algeria" },
-    { name: "Andorra" },
-    { name: "Angola" },
-    { name: "Antigua and Barbuda" },
-    { name: "Argentina" },
-    { name: "Armenia" },
-    { name: "Australia" },
-    { name: "Austria" },
-    { name: "Azerbaijan" },
-    { name: "Bahamas" },
-    { name: "Bahrain" },
-    { name: "Bangladesh" },
-    { name: "Barbados" },
-    { name: "Belarus" },
-    { name: "Belgium" },
-    { name: "Belize" },
-    { name: "Benin" },
-    { name: "Bhutan" },
-    { name: "Bolivia" },
-    { name: "Bosnia and Herzegovina" },
-    { name: "Botswana" },
-    { name: "Brazil" },
-    { name: "Brunei" },
-    { name: "Bulgaria" },
-    { name: "Burkina Faso" },
-    { name: "Burundi" },
-    { name: "Cabo Verde" },
-    { name: "Cambodia" },
-    { name: "Cameroon" },
-    { name: "Canada" },
-    { name: "Central African Republic" },
-    { name: "Chad" },
-    { name: "Chile" },
-    { name: "China" },
-    { name: "Colombia" },
-    { name: "Comoros" },
-    { name: "Congo (Congo-Brazzaville)" },
-    { name: "Congo (Congo-Kinshasa)" },
-    { name: "Costa Rica" },
-    { name: "Croatia" },
-    { name: "Cuba" },
-    { name: "Cyprus" },
-    { name: "Czechia" },
-    { name: "Denmark" },
-    { name: "Djibouti" },
-    { name: "Dominica" },
-    { name: "Dominican Republic" },
-    { name: "Ecuador" },
-    { name: "Egypt" },
-    { name: "El Salvador" },
-    { name: "Equatorial Guinea" },
-    { name: "Eritrea" },
-    { name: "Estonia" },
-    { name: "Eswatini" },
-    { name: "Ethiopia" },
-    { name: "Fiji" },
-    { name: "Finland" },
-    { name: "France" },
-    { name: "Gabon" },
-    { name: "Gambia" },
-    { name: "Georgia" },
-    { name: "Germany" },
-    { name: "Ghana" },
-    { name: "Greece" },
-    { name: "Grenada" },
-    { name: "Guatemala" },
-    { name: "Guinea" },
-    { name: "Guinea-Bissau" },
-    { name: "Guyana" },
-    { name: "Haiti" },
-    { name: "Honduras" },
-    { name: "Hungary" },
-    { name: "Iceland" },
-    { name: "India" },
-    { name: "Indonesia" },
-    { name: "Iran" },
-    { name: "Iraq" },
-    { name: "Ireland" },
-    { name: "Israel" },
-    { name: "Italy" },
-    { name: "Jamaica" },
-    { name: "Japan" },
-    { name: "Jordan" },
-    { name: "Kazakhstan" },
-    { name: "Kenya" },
-    { name: "Kiribati" },
-    { name: "Kuwait" },
-    { name: "Kyrgyzstan" },
-    { name: "Laos" },
-    { name: "Latvia" },
-    { name: "Lebanon" },
-    { name: "Lesotho" },
-    { name: "Liberia" },
-    { name: "Libya" },
-    { name: "Liechtenstein" },
-    { name: "Lithuania" },
-    { name: "Luxembourg" },
-    { name: "Madagascar" },
-    { name: "Malawi" },
-    { name: "Malaysia" },
-    { name: "Maldives" },
-    { name: "Mali" },
-    { name: "Malta" },
-    { name: "Mauritania" },
-    { name: "Mauritius" },
-    { name: "Mexico" },
-    { name: "Moldova" },
-    { name: "Monaco" },
-    { name: "Mongolia" },
-    { name: "Montenegro" },
-    { name: "Morocco" },
-    { name: "Mozambique" },
-    { name: "Myanmar (Burma)" },
-    { name: "Namibia" },
-    { name: "Nauru" },
-    { name: "Nepal" },
-    { name: "Netherlands" },
-    { name: "New Zealand" },
-    { name: "Nicaragua" },
-    { name: "Niger" },
-    { name: "Nigeria" },
-    { name: "North Korea" },
-    { name: "North Macedonia" },
-    { name: "Norway" },
-    { name: "Oman" },
-    { name: "Pakistan" },
-    { name: "Palau" },
-    { name: "Palestine" },
-    { name: "Panama" },
-    { name: "Papua New Guinea" },
-    { name: "Paraguay" },
-    { name: "Peru" },
-    { name: "Philippines" },
-    { name: "Poland" },
-    { name: "Portugal" },
-    { name: "Qatar" },
-    { name: "Romania" },
-    { name: "Russia" },
-    { name: "Rwanda" },
-    { name: "Saint Kitts & Nevis" },
-    { name: "Saint Lucia" },
-    { name: "Saint Vincent & Grenadines" },
-    { name: "Samoa" },
-    { name: "San Marino" },
-    { name: "Sao Tome & Principe" },
-    { name: "Saudi Arabia" },
-    { name: "Senegal" },
-    { name: "Serbia" },
-    { name: "Seychelles" },
-    { name: "Sierra Leone" },
-    { name: "Singapore" },
-    { name: "Slovakia" },
-    { name: "Slovenia" },
-    { name: "Solomon Islands" },
-    { name: "Somalia" },
-    { name: "South Africa" },
-    { name: "South Korea" },
-    { name: "South Sudan" },
-    { name: "Spain" },
-    { name: "Sri Lanka" },
-    { name: "Sudan" },
-    { name: "Suriname" },
-    { name: "Sweden" },
-    { name: "Switzerland" },
-    { name: "Syria" },
-    { name: "Tajikistan" },
-    { name: "Tanzania" },
-    { name: "Thailand" },
-    { name: "Timor-Leste" },
-    { name: "Togo" },
-    { name: "Tonga" },
-    { name: "Trinidad & Tobago" },
-    { name: "Tunisia" },
-    { name: "Turkey" },
-    { name: "Turkmenistan" },
-    { name: "Tuvalu" },
-    { name: "Uganda" },
-    { name: "Ukraine" },
-    { name: "United Arab Emirates" },
-    { name: "United Kingdom" },
-    { name: "United States" },
-    { name: "Uruguay" },
-    { name: "Uzbekistan" },
-    { name: "Vanuatu" },
-    { name: "Vatican City" },
-    { name: "Venezuela" },
-    { name: "Vietnam" },
-    { name: "Yemen" },
-    { name: "Zambia" },
-    { name: "Zimbabwe" },
-  ];
+
+  const handleClear = () => {
+    // sign.clear();
+    console.log("sign clear");
+  };
+
 
   const supabaseUrl = "https://xqgoqxnboybqjaqjeliq.supabase.co";
   const supabaseAnonKey =
@@ -239,27 +51,35 @@ export default function VideoSubmissionForm() {
 
   const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-  const handleClear = () => {
-    signRef.current?.clear();
-  };
-
   const handleFileUpload = async (e) => {
     const videoFile = e.target.files[0];
-    if (!videoFile) return;
+
+    if (!videoFile) {
+      console.error("No file selected");
+      return;
+    }
 
     setUploading(true);
     setUploadProgress(0);
+    console.log("Uploading video...");
 
     const fileName = uuidv4() + ".mp4";
+
     const { data, error } = await supabase.storage
       .from("videos")
       .upload(fileName, videoFile, {
         contentType: videoFile.type,
         upsert: true,
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setUploadProgress(percentCompleted);
+        },
       });
 
     if (error) {
-      console.error("Upload error:", error);
+      console.log("Error uploading video:", error);
       setUploading(false);
       setUploadSuccess(false);
       return;
@@ -275,20 +95,34 @@ export default function VideoSubmissionForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    console.log("Submit triggered");
+
     if (!agreed18 || !agreedTerms || !exclusiveRights) {
       alert("Please agree to all required checkboxes.");
       return;
     }
 
+    console.log("All required checkboxes are agreed");
+
     setLoading(true);
 
     try {
-      const trimmedCanvas = signRef.current?.getTrimmedCanvas();
-      let signatureImage = "";
+      console.log("Signature ref:", signRef);
 
+      const trimmedCanvas = signRef.current?.getTrimmedCanvas();
       if (trimmedCanvas) {
-        signatureImage = trimmedCanvas.toDataURL("image/png");
+        const base64Image = trimmedCanvas.toDataURL("image/png");
+       return base64Image
       }
+      console.log("base64Image:", base64Image);
+
+      // const signatureImage = canvas.toDataURL("image/png");
+      // console.log("Signature Image:", signatureImage);
+
+      // const trimmedCanvas = signRef.current?.getTrimmedCanvas();
+      // console.log("Trimmed canvas:", trimmedCanvas);
+      // const signatureImage = trimmedCanvas?.toDataURL("image/png");
+      // console.log("Signature Image:", signatureImage);
 
       const formData = {
         empRef: id,
@@ -300,13 +134,15 @@ export default function VideoSubmissionForm() {
         country,
         email,
         recordedVideo,
-        rawVideo,
+        rawVideo: rawVideo,
         notUploadedElsewhere,
         agreed18,
         agreedTerms,
         exclusiveRights,
-        signature: signatureImage,
+        signature: base64Image,
       };
+
+      console.log("Form Data ready to be sent:", formData);
 
       const response = await fetch(
         "https://clipflicks-admin-fe.vercel.app/api/submissions",
@@ -319,13 +155,15 @@ export default function VideoSubmissionForm() {
         }
       );
 
+      console.log("Server response:", response);
+
       if (response.status !== 200) {
         throw new Error("Failed to submit video");
       }
 
       alert("Video submitted successfully!");
 
-      // Reset form
+      // Clear fields
       setTitle("");
       setVideoURL("");
       setFirstName("");
@@ -334,13 +172,19 @@ export default function VideoSubmissionForm() {
       setCountry("");
       setEmail("");
       setVideoFiles([]);
+      setVideoURL("");
       setRecordedVideo(false);
       setNotUploadedElsewhere(false);
       setAgreed18(false);
       setAgreedTerms(false);
       setExclusiveRights(false);
+
+      if (signRef && typeof signRef.clear === "function") {
+        signRef.clear();
+        console.log("Signature cleared");
+      }
+
       setUploadSuccess(null);
-      signRef.current?.clear();
     } catch (error) {
       console.error("Submission error:", error);
       alert("Failed to submit the form. Please try again.");
